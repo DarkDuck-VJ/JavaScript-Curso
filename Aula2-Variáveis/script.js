539c306ece5d048131e5ed0e51771126afef9f37
VJ <juniormoreira263@gmail.com>
2024-07-14 14:53:07 -0300

    add aula 2 - variáveis

---
+++ Aula2-Variáveis/script.js
@@ -0,0 +1,13 @@
+//tipos primitivos 
+
+//boolean 
+var vOuF = false; 
+console.log(typeof (vOuF));
+
+//number
+var num = 1;
+console.log(typeof(num));
+
+//string
+var nome = "VJ";
+console.log(typeof(nome));
