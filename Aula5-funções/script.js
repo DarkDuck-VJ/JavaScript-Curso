d9f104fcba42cbe8a26a1f5b94b08b54dc6ed6c1
VJ <juniormoreira263@gmail.com>
2024-07-15 17:05:16 -0300

    add Aula5-funções

---
+++ Aula5-funções/script.js
@@ -0,0 +1,9 @@
+/* function função() {
+    console.log("Olá mundo!");
+}
+função(); */
+
+function mensagem(parametro1, parametro2){
+    console.log(parametro1, parametro2);
+}
+mensagem('Olá mundo', 'mensagem');
